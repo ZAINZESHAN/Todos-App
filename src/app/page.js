a6075e4a75afb170a2fb415bec5eef17296788cc
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2024-11-08 16:24:14 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col p-20 items-center h-screen">
+      <h1 className="font-bold text-6xl"> 
+        Hello World
+      </h1>
+    </div>
+  );
+}
